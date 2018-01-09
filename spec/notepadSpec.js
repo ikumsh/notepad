ac33ca1421f15dd4ef3fa81d26352f08b1d32061
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-09 09:15:15 +0000

    voodoo v.06.4 - pass/fail messages for singular tests altered

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -1,10 +1,6 @@
 var testPad = new Notepad()
-<<<<<<< HEAD
-var testNote = new Note('test')
-=======
 var testNote = new Note('doggo')
 var testNote02 = new Note('There are more than 20 characters within this note')
->>>>>>> 6057fe667d0297b3696c1b3e465bd0e87d8946c3
 
 test01 = function() {
 testEquals(testPad.notes.length,
