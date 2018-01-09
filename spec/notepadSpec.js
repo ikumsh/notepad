22ae101c50a43e4976bb560c65ab9b43390d64fd
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-09 09:03:44 +0000

    ,

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -1,6 +1,6 @@
 var notepadTest = new Test()
 var testPad = new Notepad()
-var testNote = new Note()
+var testNote = new Note('test')
 
 notepadTest.testEquals(testPad.notes.length, 0, "A notepad should initially contain no notes")
 notepadTest.testChangedBy(testPad.addNote, testNote, testPad.notes.length, 1, "Notes can be added to the notepad")
